412ea52b066bd04bdb25e1816941543118c254fe
Claire Yang <34817835+apluscs@users.noreply.github.com>
2018-11-27 17:20:17 -0500

    Create Superprime Rib.java

---
+++ Chapter 1/Superprime Rib.java
@@ -0,0 +1,101 @@
+/*Butchering Farmer John's cows always yields the best prime rib. You can tell prime ribs by looking at the digits lovingly 
+stamped across them, one by one, by FJ and the USDA. Farmer John ensures that a purchaser of his prime ribs gets really prime 
+ribs because when sliced from the right, the numbers on the ribs continue to stay prime right down to the last rib, e.g.:
+
+     7  3  3  1
+
+The set of ribs denoted by 7331 is prime; the three ribs 733 are prime; the two ribs 73 are prime, and, of course, the last rib, 
+7, is prime. The number 7331 is called a superprime of length 4.
+
+Write a program that accepts a number N 1 <=N<=8 of ribs and prints all the superprimes of that length.
+
+The number 1 (by itself) is not a prime number.
+
+INPUT FORMAT
+A single line with the number N.
+
+SAMPLE INPUT (file sprime.in)
+4
+
+OUTPUT FORMAT
+The superprime ribs of length N, printed in ascending order one per line.
+
+SAMPLE OUTPUT (file sprime.out)
+2333
+2339
+2393
+2399
+2939
+3119
+3137
+3733
+3739
+3793
+3797
+5939
+7193
+7331
+7333
+7393
+*/
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/*
+ID: the.cla1
+LANG: JAVA
+TASK: sprime
+*/
+
+public class sprime {
+
+	public static void main(String[] args) {
+		try(BufferedReader f = new BufferedReader(new FileReader("sprime.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sprime.out")));){
+			generateSupers();
+			while(f.ready()){
+				int length = Integer.parseInt(f.readLine());
+				for(int result: nums[length-1]){
+					if(result > 0) out.println(result);
+				}
+			}
+		} catch (IOException e) {}
+	}
+	
+	public static int[][] nums;
+	
+	public static void generateSupers(){
+		int currLength = 1;	int n = 0;	
+        nums = new int[8][100];
+        nums[0][0] = 2; nums[0][1] = 3; nums[0][2] = 5; nums[0][3] = 7;
+        while(currLength < 8){	//hitting all lengths
+            int i = 0;	n = 0;
+            while(nums[currLength-1][i] > 0){   //getting all relevant numbers in of length 1 digit shorter
+            	int odd = 1;
+            	while(odd < 10){	
+            		int temp = nums[currLength-1][i] * 10 + odd;	//ex. 2 --> 21, 23, 25, 27, 29
+                    if(isPrime(temp)){
+                        nums[currLength][n++] = temp;
+                    }
+                    odd += 2;
+            	}
+                i++;
+            }
+            currLength++;
+        }
+	}
+	
+	public static boolean isPrime(int num){	
+		for(int i = 2; i <= Math.sqrt(num); i++){ //already filtered out all evens
+			if(num % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+}
