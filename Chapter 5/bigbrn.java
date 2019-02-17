404d55b650510ff64029fec302e0c6c047456f1d
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-02-16 21:51:32 -0500

    Update bigbrn.java

--- Chapter 5/bigbrn.java
+++ Chapter 5/bigbrn.java
@@ -1,11 +1,3 @@
-package usaco;
-//@formatter:off
-/*
-ID: the.cla1
-LANG: JAVA
-TASK: bigbrn
-*/
-//@formatter:on
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -21,17 +13,13 @@ public class bigbrn {
   public static void main(String[] args) throws IOException {
     try (BufferedReader in = new BufferedReader(new FileReader("bigbrn.in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("bigbrn.out")));) {
-      for (int t = 1; t <= 15; t++) {
-        in.readLine();
       StringTokenizer st = new StringTokenizer(in.readLine());
       N = Integer.parseInt(st.nextToken());
       T = Integer.parseInt(st.nextToken());
       grid = new int[N + 1][N + 1];
-      for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= N; j++) {
-          grid[i][j] = -1;
-        }
-      }
+      for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++) // notice the "0" row and column are left as 0
+          grid[i][j] = -1; // obstacles will overwrite this -1
       for (int i = 0; i < T; i++) {
         st = new StringTokenizer(in.readLine());
         int r = Integer.parseInt(st.nextToken());
@@ -39,24 +27,19 @@ public class bigbrn {
         grid[r][c] = 0;
       }
       int result = dp();
-        out.println("Test #" + t + ": " + result);
-      }
+      out.println(result);
     }
-
   }
 
   public static int dp() {
     int result = 0;
-    for (int i = 1; i <= N; i++) {
+    for (int i = 1; i <= N; i++)
       for (int j = 1; j <= N; j++) {
         if (grid[i][j] == 0)
-          continue;
+          continue; // ex. if a 2 sqaure can fit left, up, and northwest to you, you can fit a 3 square
         grid[i][j] = Math.min(Math.min(grid[i - 1][j], grid[i - 1][j - 1]), grid[i][j - 1]) + 1;
         result = grid[i][j] > result ? grid[i][j] : result;
       }
-    }
-    // for (int[] gr : grid)
-    // System.out.println(Arrays.toString(gr));
     return result;
   }
 }
